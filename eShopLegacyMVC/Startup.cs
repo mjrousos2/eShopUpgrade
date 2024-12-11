456efb57fff4c796ef020154cb4684d241c96896
NoReply Amazon <no-reply@amazon.com>
2024-12-11 23:45:31 +0000

    [Commit] Iteration 18 #errors = 31 <= 32.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/eShopLegacyMVC/Startup.cs][CS0103][16,13] The name 'ConfigureAuth' does not exist in the current context``.

--- eShopLegacyMVC/Startup.cs
+++ eShopLegacyMVC/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace eShopLegacyMVC
 {
@@ -8,12 +9,19 @@ namespace eShopLegacyMVC
         public void ConfigureServices(IServiceCollection services)
         {
             // Configure services here
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/Account/Login";
+                    options.LogoutPath = "/Account/Logout";
+                });
         }
 
         public void Configure(IApplicationBuilder app)
         {
             // Configure the HTTP request pipeline here
-            ConfigureAuth(app);
+            app.UseAuthentication();
+            app.UseAuthorization();
         }
     }
 }
