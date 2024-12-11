bf8afb1a95f7c44784264792bbdcafd81881be3c
NoReply Amazon <no-reply@amazon.com>
2024-12-11 23:37:55 +0000

    [Commit] Iteration 2 #errors = 84 <= 87.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/eShopLegacyMVC/Startup.cs][CS0246][5,12] The type or namespace name 'OwinStartupAttributeAttribute' could not be found (are you missing a using directive or an assembly reference?)``.

--- eShopLegacyMVC/Startup.cs
+++ eShopLegacyMVC/Startup.cs
@@ -1,14 +1,18 @@
-using System;
-using Microsoft.AspNetCore.Owin;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
-
-[assembly: OwinStartupAttribute(typeof(eShopLegacyMVC.Startup))]
 namespace eShopLegacyMVC
 {
     public partial class Startup
     {
-        public void Configuration(IAppBuilder app)
+        public void ConfigureServices(IServiceCollection services)
         {
+            // Configure services here
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // Configure the HTTP request pipeline here
             ConfigureAuth(app);
         }
     }
